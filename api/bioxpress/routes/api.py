e366c0557bd35bdaf85eb79b2f37cfcd7c58295b
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-27 16:22:49 -0400

    refactor api routes

--- api/bioxpress/routes/api.py
+++ api/bioxpress/routes/api.py
@@ -5,21 +5,21 @@ from bioxpress.services.cancer_service import search_cancer, get_cancer_list
 api_bp = Blueprint("api", __name__)
 
 
-@api_bp.route("/stats", methods=["POST"])
+@api_bp.route("/getStats", methods=["POST"])
 def stats():
     json_data = request.get_json()
     result = get_stats(json_data)
     return jsonify(result)
 
 
-@api_bp.route("/search_cancer", methods=["POST"])
+@api_bp.route("/cancerSearch", methods=["POST"])
 def search_cancer_route():
     input_json = request.get_json()
     result = search_cancer(input_json)
     return jsonify(result)
 
 
-@api_bp.route("/get_cancer_list", methods=["GET"])
+@api_bp.route("/getCancerList", methods=["POST"])
 def get_cancer_list_route():
     result = get_cancer_list()
     return jsonify(result)
