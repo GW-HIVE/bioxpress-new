0931daee2a9fd9dd5cfdb79ef1590e8e3b80729f
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-27 16:54:12 -0400

    register routes blueprint

--- api/bioxpress/__init__.py
+++ api/bioxpress/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from bioxpress.routes.api import api_bp
 
 db = SQLAlchemy()
 
@@ -7,4 +8,5 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("bioxpress.config.Config")
     db.init_app(app)
+    app.register_blueprint(api_bp)
     return app
