4633c7cd1e1cda2c5d3627670aca97c68ab5ece3
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-27 20:49:47 -0400

    add pageconf info

--- api/bioxpress/config.py
+++ api/bioxpress/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -7,3 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv('MYSQL_CONNSTRING', 'mysql+pymysql://user:password@localhost/dbname')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    CONFIG_JSON_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.json')
+    with open(CONFIG_JSON_PATH, 'r') as config_file:
+        CONFIG_JSON = json.load(config_file)
