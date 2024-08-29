3fc73c894781c36ad684c038b219ca35d664c7ea
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-29 13:17:43 -0400

    more logging

--- api/bioxpress/routes/api.py
+++ api/bioxpress/routes/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from bioxpress.services.stats_service import get_stats
 from bioxpress.services.transcript_service import get_transcript_data, transcript_search
 from bioxpress.services.cancer_service import search_cancer, get_cancer_list
+from pprint import pprint
 
 # TODO
 # from bioxpress.services.cancer_service import search_cancer, get_cancer_list
@@ -24,7 +25,9 @@ def transcript_data():
 
 @api_bp.route("/transcriptSearch", methods=["POST"])
 def search_transcript():
+    print("HERE")
     input_json = request.get_json()
+    pprint(input_json)
     result = transcript_search(input_json)
     return jsonify(result)
 
