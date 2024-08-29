08e1c2f359cc9a6fdfc35bc09b7635dd8ed835f6
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-29 12:06:50 -0400

    add CORS

--- api/bioxpress/__init__.py
+++ api/bioxpress/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from bioxpress.db import db
 from bioxpress.routes.api import api_bp
-
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("bioxpress.config.Config")
     db.init_app(app)
+    CORS(app)
     app.register_blueprint(api_bp)
     return app
